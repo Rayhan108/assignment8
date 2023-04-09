08ee6153f560b9296e4744d60ec07ca7642de7ea
Rayhan Shorker <raihanshorker56@gmail.com>
2023-04-10 00:58:01 +0600

    banner added

--- src/Component/FirstPage/FirstPage.jsx
+++ src/Component/FirstPage/FirstPage.jsx
@@ -3,8 +3,26 @@ import React from 'react';
 const FirstPage = () => {
     return (
         <div>
-            <h1>default</h1>
+ <div className='flex bg-gray-100 mb-10'>
+         <div className='my-auto mx-auto w-96 p-11 mt-10 '>
+         <h1 className='text-6xl font-bold '>One Step Closer To Your <span className='text-purple-800'> Dream Job</span></h1>
+            <p className='mt-5'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+            <button className='bg-purple-700 text-white mt-5 text-1xl font-bold p-3 rounded-md hover:bg-purple-400'>Get Started</button>
+         </div>
+         <div>
+            <img src="person.png" alt="" />
+         </div>
         </div>
+        {/* Catagory section */}
+        <div>
+           <div className='mx-auto text-center py-3'>
+           <h1 className='text-3xl font-bold '>Job Category List</h1>
+            <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+           </div>
+           
+        </div>
+        </div>
+       
     );
 };
 
--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Bars3Icon ,XMarkIcon} from '@heroicons/react/24/solid'
 
 const Header = () => {
+    // const [open,setOpen] =useState(false)
     return (
-        <nav className='bg-gray-300 mb-10 flex justify-between p-5'>
+        <nav className='bg-gray-300  flex justify-between p-5'>
+              {/* <div onClick={()=>setOpen(!open)} className='md:hidden'>
+        <span>{open? <XMarkIcon className="h-6 w-6 text-purple-800" />  : <Bars3Icon className="h-6 w-6 text-purple-800" />  }</span>
+      </div > */}
+      {/* <div className={` md:static md:flex md:justify-between md:flex absolute  duration-500 ${open?'top-6':'-top-48'}`} >
+      </div> */}
 
-           <div className='font-bold text-3xl'>
+   
+<div className='font-bold text-3xl'>
            <h1>IT-Jobs-Portal</h1>
            </div>
      
@@ -19,8 +27,6 @@ const Header = () => {
     <div className="">
     <Link to=""><button className='bg-purple-700 text-white text-1xl font-bold p-3 rounded-md hover:bg-purple-400'>Start Applying</button></Link>
     </div>
-
-   
          
        </nav>
     );
